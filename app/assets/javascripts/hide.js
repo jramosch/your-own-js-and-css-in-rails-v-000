8d7cbaae350a64ce3461356193a1ee2c91efe8c4
Jamilya Ramos-Chapman <jamilya_ramos-chapman@alumni.brown.edu>
2019-06-17 15:26:50 +0000

    Automatically backed up by Learn

--- app/assets/javascripts/hide.js
+++ app/assets/javascripts/hide.js
@@ -1,13 +1,3 @@
-/*
-var link = document.getElementById('hide_this');
-
-function hideWhenClicked() {
-  link.style.display = "none"
-}
-
-link.addEventListener("click", hiddenWhenClicked);
-*/
-
 function hideWhenClicked(event) {
   var $target = $(event.target);
   $target.hide();
